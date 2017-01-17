ca2887be807a087892d77a5a03e4268845709bfc
Sergey Zamylin <sergeyfusezamylin@gmail.com>
2017-01-17 12:23:08 +0300

    Wrapper for SQL

--- InitModule/Wrapper.py
+++ InitModule/Wrapper.py
@@ -1,3 +1,9 @@
+import pandas as pd
+from sqlalchemy import text
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
 def get_library(lib_name):
     return __import__(lib_name)
 
@@ -19,6 +25,69 @@ def load_model(lib_name, model_name ):
     json_file.close()
     model = keras.models.model_from_json(loaded_model_json)
     return model
+
+class DataReader():
+    def __init__(self, connect):
+        self.connect = connect
+
+    def get_supervised_training_data(self, features_sql, target_sql):
+        feature_sql_raw = text(
+                                """
+                                select x1.x, x2.x, x3.x, x4.x
+                                from
+                                    (select rh.tag as tag, rh.rates_value_double as x
+                                    from rates_history rh
+                                    join rates r on rh.rates_id = r.rates_id
+                                    join category c on r.category_category_id = c.category_id
+                                    where c.category_name like :category_name and r.rates_name like :feature1) as x1
+                                join
+                                    (select rh.tag as tag, rh.rates_value_double as x
+                                    from rates_history rh
+                                    join rates r on rh.rates_id = r.rates_id
+                                    join category c on r.category_category_id = c.category_id
+                                    where c.category_name like :category_name and r.rates_name like :feature2) as x2 on x1.tag = x2.tag
+                                join
+                                    (select rh.tag as tag, rh.rates_value_double as x
+                                    from rates_history rh
+                                    join rates r on rh.rates_id = r.rates_id
+                                    join category c on r.category_category_id = c.category_id
+                                    where c.category_name like :category_name and r.rates_name like :feature3) as x3 on x1.tag = x3.tag
+                                join
+                                    (select rh.tag as tag, rh.rates_value_double as x
+                                    from rates_history rh
+                                    join rates r on rh.rates_id = r.rates_id
+                                    join category c on r.category_category_id = c.category_id
+                                    where c.category_name like :category_name and r.rates_name like :feature4) as x4 on x1.tag = x4.tag
+                                """
+                            )
+
+        X = pd.DataFrame(self.connect.execute(feature_sql_raw, features_sql).fetchall())
+        X = X.as_matrix()
+
+        target_sql_raw = text(
+                                """
+                                select rh.rates_value_char
+                                from rates_history rh
+                                join rates r on rh.rates_id = r.rates_id
+                                join category c on r.category_category_id = c.category_id
+                                where c.category_name like :category_name and r.rates_name like :target1
+
+                                """
+                        )
+
+        y = pd.DataFrame(self.connect.execute(target_sql_raw, target_sql).fetchall())
+        y = np.array(y[0], dtype=str)
+
+        return X,y
+
+    def one_hot_encoder(self, data):
+        return np.array(pd.get_dummies(data))
+
+    def train_test_split(self, X, y, train_size, random_state):
+        train_X, test_X, train_y, test_y = train_test_split(X, y,
+                                                            train_size=train_size,
+                                                            random_state=random_state)
+        return train_X, test_X, train_y, test_y
 
 class Classifier():
     def __init__(self, lib_name, model_name, model_description, model_args):
--- keras_class_iris.py
+++ keras_class_iris.py
@@ -1,13 +1,30 @@
-from InitModule.Wrapper          import Classifier
-
-from sklearn.model_selection     import train_test_split
-from pandas                      import get_dummies
+from InitModule.Wrapper          import Classifier, DataReader
 from sqlalchemy                  import create_engine, MetaData
 
-import numpy   as np
 import pandas  as pd
 import requests
 import os
+
+features_sql = {
+                    "category_name": '%Iris Attributes',
+                    "feature1": '%feature1',
+                    "feature2": '%feature2',
+                    "feature3": '%feature3',
+                    "feature4": '%feature4'
+               }
+target_sql = {
+                    "category_name": '%Iris Classes',
+                    "target1": '%target1'
+             }
+model_args = ({'neurons' : 16, 'input_dim' : 4, 'init' : 'normal', 'activation' : 'relu'},
+              {'neurons' : 3, 'input_dim' : 0, 'init' : 'normal', 'activation' : 'sigmoid'})
+compile_args = {'loss': 'categorical_crossentropy',
+                'optimizer' : 'adam',
+                'metrics' : 'accuracy'}
+fit_args = {'nb_epoch': 100,
+            'batch_size' : 1,
+            'verbose' : 0}
+evaluate_args = {'verbose' : 0}
 
 class ETL:
     def __init__(self, db_name, user='postgres', host='localhost'):
@@ -43,66 +60,15 @@ class ETL:
         meta = MetaData(bind=con, reflect=True)
         return con, meta
 
-def one_hot_encod_pandas(arr):
-    return np.array(get_dummies(arr))
-
 etl = ETL(db_name='opentrm')
 conn, meta = etl.get_metadata()
 
-features_sql = """
-            select x1.tag, x1.x, x2.x, x3.x, x4.x from
-            (select rh.tag as tag, rh.rates_value_double as x
-            from rates_history rh
-            join rates r on rh.rates_id = r.rates_id
-            join category c on r.category_category_id = c.category_id
-            where c.category_id = 2 and r.rates_id = 1) as x1
-            join
-            (select rh.tag as tag, rh.rates_value_double as x
-            from rates_history rh
-            join rates r on rh.rates_id = r.rates_id
-            join category c on r.category_category_id = c.category_id
-            where c.category_id = 2 and r.rates_id = 2) as x2 on x1.tag = x2.tag
-            join
-            (select rh.tag as tag, rh.rates_value_double as x
-            from rates_history rh
-            join rates r on rh.rates_id = r.rates_id
-            join category c on r.category_category_id = c.category_id
-            where c.category_id = 2 and r.rates_id = 3) as x3 on x1.tag = x3.tag
-            join
-            (select rh.tag as tag, rh.rates_value_double as x
-            from rates_history rh
-            join rates r on rh.rates_id = r.rates_id
-            join category c on r.category_category_id = c.category_id
-            where c.category_id = 2 and r.rates_id = 4) as x4 on x1.tag = x4.tag
-"""
+dr = DataReader(connect=conn)
+X, y = dr.get_supervised_training_data(features_sql, target_sql)
 
-X = pd.DataFrame(conn.execute(features_sql).fetchall())
-X.drop(0, inplace=True, axis=1)
-
-target_sql = """
-            select rh.rates_value_char
-            from rates_history rh
-            join rates r on rh.rates_id = r.rates_id
-            join category c on r.category_category_id = c.category_id
-            where c.category_id = 3 and r.rates_id = 5
-
-"""
-
-y = pd.DataFrame(conn.execute(target_sql).fetchall())
-
-X = X.as_matrix()
-y = np.array(y[0], dtype=str)
-
-train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.5, random_state=0)
-
-train_y_ohe = one_hot_encod_pandas(train_y)
-test_y_ohe = one_hot_encod_pandas(test_y)
-
-model_args = ({'neurons' : 16, 'input_dim' : 4, 'init' : 'normal', 'activation' : 'relu'},
-              {'neurons' : 3, 'input_dim' : 0, 'init' : 'normal', 'activation' : 'sigmoid'})
-compile_args = {'loss': 'categorical_crossentropy', 'optimizer' : 'adam', 'metrics' : 'accuracy'}
-fit_args = {'nb_epoch': 100, 'batch_size' : 1, 'verbose' : 0}
-evaluate_args = {'verbose' : 0}
+train_X, test_X, train_y, test_y = dr.train_test_split(X, y, train_size=0.5, random_state=0)
+train_y_ohe = dr.one_hot_encoder(train_y)
+test_y_ohe = dr.one_hot_encoder(test_y)
 
 clf = Classifier(lib_name='keras',
                  model_name='iris_fisher',
